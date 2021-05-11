011b857c0ee02a748e2f3ec0ead673095789969b
Alessio Bonanno <alessio.bonanno@infocube.it>
2021-05-11 13:29:41 +0200

    Aggiunte classi main e timer

--- src/main/java/timer/Main.java
+++ src/main/java/timer/Main.java
@@ -1,4 +1,25 @@
 package timer;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.start();
+
+        for (int i = 0; i<20; i++){
+            int a = 1;
+        }
+        timer.end();
+
+        //System.out.println(timer.getInitTime());
+        //System.out.println(timer.getFinTime());
+
+        //Vedo il tempo trascorso
+        System.out.println("Tempo trascorso dopo il for: " + timer.elapsedTime());
+
+        //reset del timer
+        timer.reset();
+        timer.end();
+        System.out.println("Tempo trascorso dopo il reset: " + timer.elapsedTime());
+    }
 }
--- src/main/java/timer/Timer.java
+++ src/main/java/timer/Timer.java
@@ -1,4 +1,71 @@
 package timer;
 
 public class Timer {
+
+    /**
+     * Varibili private
+     * initTime è l'inizio del timer
+     * finTime è la fine del timer
+     */
+    private long initTime;
+    private long finTime;
+
+    /**
+     * Costruttore di default
+     */
+    public Timer(){ }
+
+
+    /**
+     * Metodo che fa partire il timer
+     */
+    public void start(){
+        initTime = System.nanoTime();
+
+    }
+
+
+    /**
+     * Metodo che fa finire il timer
+     */
+    public void end(){
+        finTime = System.nanoTime();
+    }
+
+
+    /**
+     * Metodo che resetta il timer
+     */
+    public void reset(){
+        initTime = System.nanoTime();
+        finTime = 0;
+    }
+
+
+    /**
+     * Metodo per il calcolo del tempo trascorso
+     * @return Il valore del tempo trascorso dalla partenza del timer alla sua fine
+     */
+    public long elapsedTime(){
+        return this.finTime - this.initTime;
+    }
+
+
+    /**
+     * Getter per initTime
+     * @return Il suo valore
+     */
+    public long getInitTime() {
+        return initTime;
+    }
+
+    /**
+     * Getter per finTime
+     * @return IL suo valore
+     */
+    public long getFinTime() {
+        return finTime;
+    }
+
+
 }
